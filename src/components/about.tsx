5385a3c3219b59ed76ded543c1cc9dc86371acad
matheusAI <ms2279584@gmail.com>
2024-10-23 16:48:12 -0300

    add icons

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,26 +2,71 @@
 
 import React from "react";
 import Image from "next/image";
+import { FaEnvelope, FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 const About: React.FC = () => {
   return (
     <section id="#sobre" className=" text-gray-300 py-16 px-8">
       <div className="container grid md:grid-cols-2 grid-cols-1 gap-4 mx-auto justify-center items-center">
         <div className=" flex flex-col items-center justify-center py-3 gap-2">
-            <Image src="/image/perfil.jpg" alt="" width={320} height={320} className=" rounded-full"/>
-            <div className=" w-80 h-16 bg-[#121215] rounded-full"></div>
+          <Image
+            src="/image/perfil.jpg"
+            alt=""
+            width={320}
+            height={320}
+            className=" rounded-full p-2 bg-gradient-to-b from-red-600 to-red-950"
+          />
+          <div className=" w-96 h-16 bg-[#121215] rounded-full flex items-center justify-around">
+            {/* Gmail */}
+            <a href="mailto:ms2279584@gmail.com" aria-label="Gmail">
+              <FaEnvelope size={40} />
+            </a>
+
+            {/* LinkedIn */}
+            <a
+              href="https://www.linkedin.com/in/matheus-santos-434ab0200/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin size={40} />
+            </a>
+
+            {/* GitHub */}
+            <a
+              href="https://github.com/matheusAI"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+            >
+              <FaGithub size={40} />
+            </a>
+
+            {/* Instagram */}
+            <a
+              href="https://www.instagram.com/matheus_3341/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram"
+            >
+              <FaInstagram size={40} />
+            </a>
+          </div>
         </div>
         <div className="container">
           <h2 className="text-4xl font-bold text-white mb-6">sobre mim</h2>
           <p className="text-lg leading-relaxed mb-4">
-          sou Desenvolvedor de software especializado em criar soluções web dinâmicas e escaláveis 
-          com tecnologias como Nest.js, Next.js com Node.js. 
-          Focado na construção de aplicações eficientes e centradas na experiência do usuário.
+            sou Desenvolvedor de software especializado em criar soluções web
+            dinâmicas e escaláveis com tecnologias como Nest.js, Next.js com
+            Node.js. Focado na construção de aplicações eficientes e centradas
+            na experiência do usuário.
           </p>
           <p className="text-lg leading-relaxed">
-          Ao longo da minha carreira, venho aperfeiçoando minhas habilidades em TypeScript, Tailwind CSS e
-          outras ferramentas essenciais para entregar produtos robustos e de alto desempenho. 
-          Estou constantemente em busca de inovações tecnológicas para criar soluções eficazes que atendam às necessidades do cliente.
+            Ao longo da minha carreira, venho aperfeiçoando minhas habilidades
+            em TypeScript, Tailwind CSS e outras ferramentas essenciais para
+            entregar produtos robustos e de alto desempenho. Estou
+            constantemente em busca de inovações tecnológicas para criar
+            soluções eficazes que atendam às necessidades do cliente.
           </p>
         </div>
       </div>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-[#121215] text-gray-300 py-4">
       <div className="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} MyPortfolio. All rights reserved.</p>
-        <div className="mt-2 space-x-4">
-          <a href="https://github.com/matheusAI" target="_blank" rel="noopener noreferrer" className="hover:text-white">GitHub</a>
-          <a href="https://www.linkedin.com/in/matheus-santos-434ab0200/" target="_blank" rel="noopener noreferrer" className="hover:text-white">LinkedIn</a>
+        <p>
+          &copy; {new Date().getFullYear()} MyPortfolio. All rights reserved.
+        </p>
+        <div className="mt-2 space-x-4 flex justify-center ">
+          <a
+            href="https://github.com/matheusAI"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+            className="hover:text-white"
+          >
+            <FaGithub size={30} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/matheus-santos-434ab0200/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+            className="hover:text-white"
+          >
+            <FaLinkedin size={30} />
+          </a>
         </div>
       </div>
     </footer>
