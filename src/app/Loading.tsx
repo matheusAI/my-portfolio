c8c8d277eda0caca4dff589d42a825dc8590d00c
matheusAI <ms2279584@gmail.com>
2024-10-25 20:42:03 -0300

    FEAT-Loading

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,4 +1,13 @@
 export default function Loading() {
-    return <div>Carregando...</div>;
-  }
-  
+  return (
+    <div className="relative w-[200px] h-[60px] z-[1]">
+      <div className="absolute w-[20px] h-[20px] left-[15%] bg-white rounded-full animate-circle"></div>
+      <div className="absolute w-[20px] h-[20px] left-[45%] bg-white rounded-full animate-circle delay-[0.2s]"></div>
+      <div className="absolute w-[20px] h-[20px] right-[15%] bg-white rounded-full animate-circle delay-[0.3s]"></div>
+
+      <div className="absolute w-[20px] h-[4px] left-[15%] bg-black rounded-full blur-[1px] opacity-90 animate-shadow z-[-1]"></div>
+      <div className="absolute w-[20px] h-[4px] left-[45%] bg-black rounded-full blur-[1px] opacity-90 animate-shadow delay-[0.2s] z-[-1]"></div>
+      <div className="absolute w-[20px] h-[4px] right-[15%] bg-black rounded-full blur-[1px] opacity-90 animate-shadow delay-[0.3s] z-[-1]"></div>
+    </div>
+  );
+}
