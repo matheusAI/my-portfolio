bd9af546d26ede3f39aabeaad120765fc86b822f
matheusAI <ms2279584@gmail.com>
2024-10-25 22:05:38 -0300

    FEAT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Projetos from "@/components/projetos/projetos";
 
 export default function Home() {
   return (
-    <main className=" flex flex-col gap-10">
+    <main className=" flex flex-col gap-10 items-center ">
       <section id="sobre">
         <About />
       </section>
+      <div className="p-1 w-[90%] bg-[#121215]"></div>
       <section id="projetos">
         <Projetos />
       </section>
